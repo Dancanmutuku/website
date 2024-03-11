283e33011ee7f18d1556bc6e4ea65c029dd960f1
Dancanmutuku <133522453+Dancanmutuku@users.noreply.github.com>
2024-03-11 22:40:31 +0300

    new app

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    echo"i love Valentina Kerubo
+    <br> But she is dramatic
+    <br><br>"
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Beat Val</button>
+</body>
+</html>
